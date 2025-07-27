3a3e77fdfd81ec86ac2b89c66ecedec2a85a6d0e
abhinarwal0001@gmail.com <abhinarwal0001@gmail.com>
2025-07-27 17:06:54 +0530

    Removed lint errors

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
 import { test } from 'vitest'; // Import test function
 import App from '../App';
 
 test('renders a component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Vite \+ React/i);
+  // const linkElement = screen.getByText(/Vite \+ React/i);
   // ... rest of your test assertions
 });
